b073ff5348020825e94389999673d5ded4bd839e
KevinCaballeroP <160201775+KevinCaballeroP@users.noreply.github.com>
2025-11-11 16:01:21 -0500

    imagen icono

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,15 @@
 // app/layout.js
 export const metadata = {
-  title: "Novelas App",
+  title: "🌌 SPT Novelas",
   description: "Lector de novelas y mangas",
+ icons: {
+  icon: [
+    { url: "/favicon.ico", type: "image/x-icon" },
+    { url: "/favicon.png", type: "image/png" },
+  ],
+},
+
+
 };
 
 export default function RootLayout({ children }) {
